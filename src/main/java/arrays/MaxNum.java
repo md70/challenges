b84baec67bc1f532d1982336169eabc5de411ac3
Mahmoud <mahmoud.saeed@directfn.com>
2016-04-03 18:54:45 +0200

    commit the changes of the find max number

--- src/main/java/arrays/MaxNum.java
+++ src/main/java/arrays/MaxNum.java
@@ -5,13 +5,21 @@ package arrays;
  * Using traditional java and Java 8 new APIs
  */
 
-import java.util.*;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import rx.Observable;
+import rx.observables.MathObservable;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 public class MaxNum {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 6, 7};
-        System.out.println(getMax1(arr));
+        Integer[] arr = {3, 5, 6, 7};
+        getMax5(arr);
+//        System.out.println(getMax5(arr));
     }
 
     /**
@@ -20,7 +28,7 @@ public class MaxNum {
      * @return the max number in the array
      */
     private static int getMax1(int[] arr) {
-        int max = arr[1];
+        int max = arr[0];
         for (int i = 1; i < arr.length; i++)
             if (arr[i] > max)
                 max = arr[i];
@@ -38,7 +46,7 @@ public class MaxNum {
     }
 
     /**
-     * Another way using Java 8
+     * Using Java 8
      * @param arr of integers
      * @return the max number in the array
      */
@@ -47,12 +55,31 @@ public class MaxNum {
     }
 
     /**
-     * Another way using Java Collections utils
+     * Using Java Collections utils
      * @param arr of integers
      * @return the max number in the array
      */
     private static int getMax4(Integer[] arr) {
         return Collections.max(Arrays.asList(arr));
     }
+
+    /**
+     * Using RxJava
+     * @param arr array of integers
+     */
+    private static void getMax5(Integer[] arr) {
+        Observable<Integer> findMax = Observable.from(Arrays.asList(arr));
+        MathObservable.max(findMax).subscribe(System.out::println);
+    }
+
+    /**
+     * Using Apache Commons
+     * @param arr array of integers
+     * @return the max number
+     */
+    private static int getMax6(int[] arr) {
+        return NumberUtils.max(arr);
+    }
+
 
 }
