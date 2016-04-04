07f7052aea7cbc0906af27e7592d2bfbc1b8d163
Mahmoud <mahmoud.saeed@directfn.com>
2016-04-04 19:27:26 +0200

    added merge class to merge two arrays

---
+++ src/main/java/array/Merge.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+
+public class Merge {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {5, 6, 7};
+        int[] merged = merge(arr1, arr2);
+        System.out.println(Arrays.toString(merged));
+    }
+
+    private static int[] merge(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+
+        for (int i = 0; i< c.length; i++)
+            if (i < a.length)
+                c[i] = a[i];
+            else
+                c[i] = b[i - a.length];
+
+        return c;
+    }
+}
