a2ab985ee1dee772a2fd119e6fa1ffdea2e3fa0a
Mahmoud <mahmoud.saeed@directfn.com>
2016-04-04 19:33:35 +0200

    renamed the Merge class to Concat

--- src/main/java/array/Concat.java
+++ src/main/java/array/Concat.java
@@ -2,15 +2,15 @@ package array;
 
 import java.util.Arrays;
 
-public class Merge {
+public class Concat {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4};
         int[] arr2 = {5, 6, 7};
-        int[] merged = merge(arr1, arr2);
+        int[] merged = concat(arr1, arr2);
         System.out.println(Arrays.toString(merged));
     }
 
-    private static int[] merge(int[] a, int[] b) {
+    private static int[] concat(int[] a, int[] b) {
         int[] c = new int[a.length + b.length];
 
         for (int i = 0; i< c.length; i++)
