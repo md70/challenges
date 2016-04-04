1ac9c662c586411855291f4e316156757abb5354
Mahmoud <mahmoud.saeed@directfn.com>
2016-04-04 18:56:43 +0200

    removed more braces

--- src/main/java/array/NoDups.java
+++ src/main/java/array/NoDups.java
@@ -58,20 +58,19 @@ public class NoDups {
         int j = 0;
         int i = 1;
         //return if the array length is less than 2
-        if(input.length < 2){
+        if(input.length < 2)
             return input;
-        }
-        while(i < input.length){
-            if(input[i] == input[j]){
+
+        while(i < input.length)
+            if(input[i] == input[j])
                 i++;
-            }else{
+            else
                 input[++j] = input[i++];
-            }
-        }
+
         int[] output = new int[j+1];
-        for(int k=0; k<output.length; k++){
+        for(int k=0; k<output.length; k++)
             output[k] = input[k];
-        }
+
         return output;
     }
 }
