fed31f91cedbd51d31e6918c3f12b1f82eaa8864
Mahmoud <mahmoud.saeed@directfn.com>
2016-06-14 10:54:49 +0200

    MRP-00 added the search method for binary search

--- src/main/java/arrays/Searching.java
+++ src/main/java/arrays/Searching.java
@@ -5,13 +5,38 @@ import java.util.stream.IntStream;
 
 public class Searching {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println(indexOf4(arr, 7));
+        int[] arr = {1, 5, 5, 5, 5, 6, 7};
+        System.out.println(indexOf(arr, 5));
+    }
+
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param  a the array of integers, must be sorted in ascending order
+     * @param  key the search key
+     * @return index of key in array <tt>a</tt> if present; <tt>-1</tt> otherwise
+     */
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else {  // Key Found!
+                while (mid > 0 && a[mid] == a[mid-1])
+                    mid--;
+                return mid;
+            }
+        }
+        return -1;
     }
 
 
     /**
      * Binary search simple implementation
+     *
      * @param a sorted array
      * @param key to find it
      * @return the index of key
@@ -20,7 +45,7 @@ public class Searching {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi) {
-            int mid = (lo + hi) >>> 1;              // === lo + (hi - lo) / 2  === (lo + hi) / 2
+            int mid = (lo + hi) >>> 1;              // = lo + (hi - lo) / 2  = (lo + hi) / 2
             if      (key < a[mid]) hi = mid - 1;
             else if (key > a[mid]) lo = mid + 1;
             else return mid;
@@ -41,6 +66,7 @@ public class Searching {
 
     /**
      * Traditional/way to search for
+     *
      * @param a
      * @param key
      * @return
@@ -53,6 +79,7 @@ public class Searching {
 
     /**
      * Modern way to search for element using linear search
+     *
      * @param a an array of ordered/unordered
      * @param key the element to search for it
      * @return the index of key if found or -1 if not found
